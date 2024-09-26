540a5b49245f24a582878795a8452cee3b5f6aab
Hunter Curren <hcurren82@gmail.com>
2024-09-26 15:27:44 -0700

    Add appsettings and some boilerplate to the main program.cs

--- InventoryManager/Program.cs
+++ InventoryManager/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace InventoryManager
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace InventoryManager
 {
     internal class Program
     {
+        private static IConfigurationRoot _configuration;
+        //private static DbContextOptionsBuilder<InventoryDbContext> _optionsBuilder;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+        }
+
+        static void BuildOptions()
+        {
+            _configuration = ConfigBuilder.ConfigurationRoot;
+            //_optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
+            //_optionsBuilder.UseSqlServer(_configuration.GetConnectionString("InventoryManager"));
         }
     }
 }

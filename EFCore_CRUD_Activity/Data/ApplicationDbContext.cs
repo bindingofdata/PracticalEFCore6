767885c8b2ab99e6d420af9a136e7f4f7b582fd4
Hunter Curren <hcurren82@gmail.com>
2024-10-16 15:33:51 -0700

    Added models and generated table creation migration.

--- EFCore_CRUD_Activity/Data/ApplicationDbContext.cs
+++ EFCore_CRUD_Activity/Data/ApplicationDbContext.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using InventoryModels;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EFCore_CRUD_Activity.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryDetail> CategoryDetails { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {

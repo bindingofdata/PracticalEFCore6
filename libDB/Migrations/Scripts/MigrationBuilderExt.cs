9b34a94386a348625d83568d5f654696bf804f3e
Hunter Curren <hcurren82@gmail.com>
2024-10-22 15:19:41 -0700

    Completed task to create a stored procedure from a SQL file.

---
+++ libDB/Migrations/Scripts/MigrationBuilderExt.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations.Builders;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libDB.Migrations.Scripts
+{
+    public static class MigrationBuilderExt
+    {
+        public static OperationBuilder<SqlOperation> SqlResource(this MigrationBuilder builder, string relativeFilePath)
+        {
+            using (Stream? stream = Assembly.GetAssembly(typeof(MigrationBuilderExt))
+                .GetManifestResourceStream(relativeFilePath))
+            {
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    stream.CopyTo(memoryStream);
+                    byte[] data = memoryStream.ToArray();
+                    string text = Encoding.UTF8.GetString(data, 3, data.Length - 3);
+                    return builder.Sql(text);
+                }
+            }
+        }
+    }
+}

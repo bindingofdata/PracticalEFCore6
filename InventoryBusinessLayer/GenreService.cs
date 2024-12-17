e5cedc0a348cea45e9ff2725685aa4ed41fb6a42
Hunter Curren <hcurren82@gmail.com>
2024-12-17 13:56:54 -0800

    Added business layer support for adding/deleting genres.

---
+++ InventoryBusinessLayer/GenreService.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+
+using InventoryModels;
+using InventoryModels.Dtos;
+
+using libDB;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDatabaseLayer
+{
+    public class GenreService : IGenresService
+    {
+        private readonly GenresRepo _dbRepo;
+
+        public GenreService(InventoryDbContext context, IMapper mapper)
+        {
+            _dbRepo = new GenresRepo(context, mapper);
+        }
+
+        public void DeleteGenre(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("Please set a valid ID");
+            }
+
+            _dbRepo.DeleteGenre(id);
+        }
+
+        public void DeleteGenres(List<int> genreIds)
+        {
+            try
+            {
+                _dbRepo.DeleteGenres(genreIds);
+            }
+            catch (Exception ex)
+            {
+                // TODO: better logging/not squelching
+                Debug.WriteLine($"The transaction has failed: {ex.Message}");
+                throw;
+            }
+        }
+
+        public List<GenreDto> GetGenres()
+        {
+            return _dbRepo.GetGenres();
+        }
+
+        public int UpsertGenre(Genre genre)
+        {
+            return _dbRepo.UpsertGenre(genre);
+        }
+
+        public void UpsertGenres(List<Genre> genres)
+        {
+            try
+            {
+                _dbRepo.UpsertGenres(genres);
+            }
+            catch (Exception ex)
+            {
+                // TODO: better logging/not squelching
+                Debug.WriteLine($"The transaction has failed: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
